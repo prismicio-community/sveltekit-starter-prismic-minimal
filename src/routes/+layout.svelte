44df68c75404072d38f040054c4a749f7bb8098a
lihbr <lihbr@users.noreply.github.com>
2025-05-14 15:16:08 +0200

    feat!: update to Svelte 5

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,19 @@
 <script>
 	import { PrismicPreview } from '@prismicio/svelte/kit';
-	import { page } from '$app/stores';
+	import { page } from '$app/state';
 	import { repositoryName } from '$lib/prismicio';
 </script>
 
 <svelte:head>
-	<title>{$page.data.title}</title>
-	{#if $page.data.meta_description}
-		<meta name="description" content={$page.data.meta_description} />
+	<title>{page.data.title}</title>
+	{#if page.data.meta_description}
+		<meta name="description" content={page.data.meta_description} />
 	{/if}
-	{#if $page.data.meta_title}
-		<meta name="og:title" content={$page.data.meta_title} />
+	{#if page.data.meta_title}
+		<meta name="og:title" content={page.data.meta_title} />
 	{/if}
-	{#if $page.data.meta_image}
-		<meta name="og:image" content={$page.data.meta_image} />
+	{#if page.data.meta_image}
+		<meta name="og:image" content={page.data.meta_image} />
 		<meta name="twitter:card" content="summary_large_image" />
 	{/if}
 </svelte:head>
